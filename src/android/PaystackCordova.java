040f69603852ca5a1d6d881645100ec18ac2d9e1
Patrick Foh <patrickfoh@gmail.com>
2018-09-26 23:56:07 +0100

    Fix syntax error

--- src/android/PaystackCordova.java
+++ src/android/PaystackCordova.java
@@ -243,7 +243,7 @@ public class PaystackCordova extends CordovaPlugin {
                 }
 
                 if (transaction_charge >= 0) {
-                    charge.setTransactionCharge(transaction_charge;
+                    charge.setTransactionCharge(transaction_charge);
                 }
                 else {
                     handleError("Invalid transaction_charge.", 1);
